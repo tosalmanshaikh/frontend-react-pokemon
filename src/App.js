e303a692168de5f4b08526b6de4bf8373d1236f3
tosalmanshaikh <tosalmanshaikh@gmail.com>
2021-12-13 10:08:14 +0100

    final-sumit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,216 @@
-import React from 'react';
+//working: for one pokemon - :)
+
+// import React, {useEffect, useState} from 'react';
+// import './App.css';
+// import axios from "axios";
+// // import Button from "./assets/components/button";
+// import PokemonCard from "./assets/components/PokemonCard";
+// // import Hans from "./assets/components/Hans";
+//
+// function App() {
+//
+//     const [pokemonData, setPokemonData] = useState('');
+//     const [endPoint, setEndPoint] = useState('https://pokeapi.co/api/v2/pokemon/jigglypuff')
+//     const [counter, setCounter] = useState(0);
+//
+//
+//     useEffect(() => {
+//
+//         async function fetchData() {
+//             try {
+//                 const result = await axios.get(endPoint);
+//                 console.log(result.data);
+//                 setPokemonData(result.data)
+//             } catch (e) {
+//                 console.error(e);
+//             }
+//         }
+//
+//         fetchData()
+//         console.log("we gaan beginnen :)")
+//     }, [endPoint])
+//
+//
+//     return (
+//         <div>
+//             {/*<Hans/>*/}
+//             <PokemonCard endPoint={endPoint}/>
+//
+//             {/*<Button isDisabled={true} type="button"*/}
+//             {/*        clickHandler={() => setCounter(pokemonData.previous)}> vorige </Button>*/}
+//             {/*<Button isDisabled={false} type="button"*/}
+//             {/*        clickHandler={() => setCounter(pokemonData.next)}> volgende </Button>*/}
+//
+//         </div>
+//     );
+// }
+//
+// export default App;
+
+
+
+
+//working: for 20 pokemon - :)
+
+import axios from "axios";
 import './App.css';
+import React, {useState, useEffect} from "react";
+import PokemonCard from "./assets/components/PokemonCard";
 
 function App() {
-  return (
-    <div>
-      Begin hier met de opdracht!
-    </div>
-  );
+    const [pokemonData, setPokemonData] = useState('');
+    const [endPoint, setEndPoint] = useState(`https://pokeapi.co/api/v2/pokemon`); // hier we give the website base as a start reference
+
+
+    //why? useEffect: you tell React that your component needs to do something after render. React will remember the function you passed (we’ll refer to it as our “effect”), and call it later after performing the DOM updates. In this effect, we set the document title, but we could also perform data fetching or call some other imperative API.
+    useEffect(() => {
+
+        async function fetchData() {
+            try {
+                // here we mean with (endpoint) our base url as  start point
+                const result = await axios.get(endPoint)
+                console.log("hier halen we data binnen: naam, url? --> 2");
+                console.log(result.data);
+                setPokemonData(result.data);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+        fetchData()
+        console.log("we gaan beginnen :D --> 1");
+    }, [endPoint])
+
+    // start reference point?
+    // here we give endpoint as dependency otherwise it will load only the first time and stop after!
+
+    return (
+
+        <div>
+            <span className="buttons">
+            <button type="button" onClick={() => setEndPoint(pokemonData.previous)}>Vorige</button>
+            <button type="button" onClick={() => setEndPoint(pokemonData.next)}>Volgende</button>
+            </span>
+
+            {/* pokemonData &&: if it is  pokemonData than go on further */}
+            {/* --> we pick up pokemonData.results and map this to be able to filter name and url */}
+
+            {pokemonData && <>
+                {pokemonData.results.map((pokemon) => {
+                    return (
+
+                        //<PokemonCard: typical so you can reuse this is PokemonCard.js
+                        // we are able to filter name and url now because we mappen "results" earlier
+                        //key= {pokemon.name} - because it is unique?
+
+                        <PokemonCard key={pokemon.name} banana={pokemon.url}/>
+                    )
+                })}
+
+            </>}
+        </div>
+    );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//hoe kunnen we dit voor alle 20?
+
+// import React, {useEffect, useState} from 'react';
+// import './App.css';
+// import axios from "axios";
+// import Button from "./assets/components/button";
+// import PokemonCard from "./assets/components/PokemonCard";
+//
+// function App() {
+//
+//     const [pokemonData, setPokemonData] = useState('');
+//     const [endPoint, setEndPoint] = useState('https://pokeapi.co/api/v2/pokemon/')
+//     const [counter, setCounter] = useState(0);
+//
+//
+//     useEffect(() => {
+//
+//         async function fetchData() {
+//             try {
+//                 const input = await axios.get(endPoint);
+//                 console.log(input.data.results);
+//                 setPokemonData(input.data.results)
+//             } catch (e) {
+//                 console.error(e);
+//             }
+//         }
+//
+//         fetchData()
+//         console.log("we gaan beginnen :)")
+//     }, [])
+//
+//
+//     return (
+//         <div>
+//
+//
+//             {pokemonData.results.map((pokemon) => {
+//                 return (
+//
+//                     <li>
+//                         {pokemon.name}
+//                     </li>
+//                 )
+//
+//             })}
+//
+//             <PokemonCard endPoint={endPoint}/>
+//
+//             {/*<Button isDisabled={true} type="button"*/}
+//             {/*        clickHandler={() => setCounter(pokemonData.previous)}> vorige </Button>*/}
+//             {/*<Button isDisabled={false} type="button"*/}
+//             {/*        clickHandler={() => setCounter(pokemonData.next)}> volgende </Button>*/}
+//
+//         </div>
+//     );
+// }
+
+// export default App;
+
+
---
+++ src/assets/components/PokemonCard.js
@@ -0,0 +1,298 @@
+// working: for one pokemon - :)
+
+// import React, {useEffect, useState} from 'react';
+// import axios from "axios";
+//
+//
+//
+// function PokemonCard ({endPoint} ) {
+//
+//     const [pokemonData, setPokemonData] = useState('');
+//
+//     useEffect(() => {
+//
+//         async function fetchData() {
+//             try {
+//                 const result = await axios.get(endPoint);
+//                 console.log(result.data);
+//                 setPokemonData(result.data)
+//             } catch (e) {
+//                 console.error(e);
+//             }
+//         }
+//
+//         fetchData()
+//         // console.log("we gaan beginnen :)")
+//     }, [])
+//
+//
+//     return (
+//         <div>
+//             {pokemonData && <> <h3>{pokemonData.name}</h3>
+//                 <img alt="jigglypuff-image" src={pokemonData.sprites.front_default}/>
+//                 <p>weight: {pokemonData.weight}</p>
+//                 <p>moves: {pokemonData.moves.length}</p>
+//
+//                 {pokemonData.abilities.map((pokemon) => {
+//                     return (
+//
+//                         <li>
+//                             {pokemon.ability.name}
+//                         </li>
+//                     )
+//
+//                 })}
+//
+//             </> }
+//
+//
+//             {/*<Button isDisabled={true} type="button"*/}
+//             {/*        clickHandler={() => setCounter(pokemonData.previous)}> vorige </Button>*/}
+//             {/*<Button isDisabled={false} type="button"*/}
+//             {/*        clickHandler={() => setCounter(pokemonData.next)}> volgende </Button>*/}
+//         </div>
+//     );
+// }
+//
+// export default PokemonCard;
+
+
+
+
+
+//working: for 20 pokemon - :)
+
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+
+//banana: now we are going to use the URL that we already fetched to go one step depper to be get more data?
+function PokemonCard  ({banana})  {
+
+    const [pokemonData, setPokemonData] = useState('');
+
+    //useEffect: always on top of everything!
+    useEffect(() => {
+
+        async function fetchData() {
+            try {
+                const result = await axios.get(banana)
+                console.log(" hier gaan we door de url om meer informatie uit te halen zoals weight, moves, abilities --> 4 ?")
+                console.log(result.data);
+                setPokemonData(result.data);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+        fetchData()
+        console.log('start! --> 3');
+    }, [])
+
+    return (
+        <div className="pokemon-data-1">
+            {pokemonData && <>
+                <h3>{pokemonData.name}</h3>
+                <img src={pokemonData.sprites.front_default} alt="pokemon-image"/>
+                <h4>Weight</h4>
+                {pokemonData.weight}
+                <h4>Moves</h4>
+                {pokemonData.moves.length}
+                <h4>Abilities</h4>
+                {/*// we are going to map through abilities to refine name(ability) */}
+                {pokemonData.abilities.map((pokemon) => {
+                    return (
+                        <li className="pokemon-data-2">{pokemon.ability.name}</li>) // .name here means ability -
+                })}
+            </>}
+        </div>
+    )
+}
+export default PokemonCard;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//hoe verdeer?
+// import React, {useEffect, useState} from 'react';
+// import axios from "axios";
+//
+//
+// function PokemonCard ({endPoint} ) {
+//
+//     const [pokemonData, setPokemonData] = useState([]);
+//
+//     useEffect(() => {
+//
+//         async function fetchData() {
+//             try {
+//                 const result = await axios.get(endPoint);
+//                 console.log(result.data);
+//                 setPokemonData(result.data)
+//             } catch (e) {
+//                 console.error(e);
+//             }
+//         }
+//
+//         fetchData()
+//         // console.log("we gaan beginnen :)")
+//     }, [])
+//
+//
+//     return (
+//         <div>
+//             {pokemonData.results && <> <h3>{pokemonData.name}</h3>
+//                 <img alt="jigglypuff-image" src={pokemonData}/>
+//                 <p>weight: {pokemonData.weight}</p>
+//                 <p>moves: {pokemonData.moves.length}</p>
+//
+//                 {pokemonData.results.map((pokemon) => {
+//                     return (
+//
+//                         <li>
+//                             {pokemon.name}
+//                         </li>
+//                     )
+//
+//                 })}
+//
+//             </> }
+//
+//
+//             {/*<Button isDisabled={true} type="button"*/}
+//             {/*        clickHandler={() => setCounter(pokemonData.previous)}> vorige </Button>*/}
+//             {/*<Button isDisabled={false} type="button"*/}
+//             {/*        clickHandler={() => setCounter(pokemonData.next)}> volgende </Button>*/}
+//         </div>
+//     );
+// }
+//
+// export default PokemonCard;
+
+
+
+
+
+//  NOVA
+
+
+// import React, { useState, useEffect, useContext } from 'react';
+// import {
+//     BrowserRouter as Router,
+//     Switch,
+//     Route,
+// } from 'react-router-dom';
+// import axios from 'axios';
+// import SearchBar from './components/searchBar/SearchBar';
+// import TabBarMenu from './components/tabBarMenu/TabBarMenu';
+// import MetricSlider from './components/metricSlider/MetricSlider';
+// import ForecastTab from './pages/forecastTab/ForecastTab';
+// import TodayTab from './pages/todayTab/TodayTab';
+// import { TempContext } from './context/TempContext';
+// import './App.css';
+//
+// const apiKey = '80da339f580ee3d5a0cdea6c86ecb847';
+//
+// function App() {
+//     const [weatherData, setWeatherData] = useState({});
+//     const [location, setLocation] = useState('');
+//     const [error, toggleError] = useState(false);
+//
+//     const { kelvinToMetric } = useContext(TempContext);
+//
+//     useEffect(() => {
+//         async function fetchData() {
+//             toggleError(false);
+//
+//             try {
+//                 const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location},nl&appid=${apiKey}&lang=nl`);
+//                 console.log(result.data);
+//                 setWeatherData(result.data);
+//             } catch (e) {
+//                 console.error(e);
+//                 toggleError(true);
+//             }
+//         }
+//
+//         if (location) {
+//             fetchData();
+//         }
+//     }, [location]);
+//
+//     return (
+//         <>
+//             <div className="weather-container">
+//
+//                 {/*HEADER -------------------- */}
+//                 <div className="weather-header">
+//                     <SearchBar setLocationHandler={setLocation}/>
+//                     {error &&
+//                         <span className="wrong-location-error">
+//               Oeps! Deze locatie bestaat niet
+//             </span>
+//                     }
+//
+//                     <span className="location-details">
+//             {Object.keys(weatherData).length > 0 &&
+//                 <>
+//                     <h2>{weatherData.weather[0].description}</h2>
+//                     <h3>{weatherData.name}</h3>
+//                     <h1>{kelvinToMetric(weatherData.main.temp)}</h1>
+//                 </>
+//             }
+//           </span>
+//                 </div>
+//
+//                 {/*CONTENT ------------------ */}
+//                 <Router>
+//                     <div className="weather-content">
+//                         <TabBarMenu/>
+//
+//                         <div className="tab-wrapper">
+//                             <Switch>
+//                                 <Route path="/komende-week">
+//                                     <ForecastTab coordinates={weatherData.coord}/>
+//                                 </Route>
+//                                 <Route path="/" exact>
+//                                     <TodayTab coordinates={weatherData.coord}/>
+//                                 </Route>
+//                             </Switch>
+//                         </div>
+//                     </div>
+//                 </Router>
+//
+//                 <MetricSlider/>
+//             </div>
+//         </>
+//     );
+// }
+//
+// export default App;
